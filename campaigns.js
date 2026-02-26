11abd75c755c8e42177550204404f7ab9886ad81
ahmedammad1709 <ttorfidoo001@gmail.com>
2026-02-27 00:04:14 +0500

    fixed patch issue

--- campaigns.js
+++ campaigns.js
@@ -60,7 +60,7 @@ const corsOptions = {
     "https://campaigns-staging.vercel.app",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
